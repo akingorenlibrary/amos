7ceed792efd2a64e39fb6da5babe415bca14e842
betulbodurrr <bet.bodur@gmail.com>
2024-05-18 12:38:23 +0300

    #düzeltme

--- makaleonerisistemi/accounts/views.py
+++ makaleonerisistemi/accounts/views.py
@@ -90,13 +90,14 @@ def dashboard(request):
         if user_data:
             ialanlari = str(user_data.get('ilgi_alanlari'))
             arama=str(user_data.get('searchKeys'))
-            if arama:
+            if arama!="None":
                 ialanlari = f"{ialanlari}, {arama}"
-            #print("ialan+arama:-",ialanlari)
 
             #processed_textF = kullaniciVektorF(ialanlari)
             #processed_textS = kullaniciVektorS(ialanlari)
             documents = makale_collection.find()
+            user_vector = []
+            user_vector1 = []
             user_vector = kullaniciVektorF(ialanlari)
             user_vector1 = kullaniciVektorS(ialanlari)
             similarity_scores = []
@@ -104,14 +105,13 @@ def dashboard(request):
             similarity_id = []
             similarity_id2 = []
             oneri=[]
-            
             if user_vector is not None and user_vector1 is not None:
                 for article_vector in documents:
                     ft = article_vector.get("fasttext", [])
+                    st = article_vector.get("scibert", [])
                     similarity_score = cosine_similarity(ft, user_vector)
                     similarity_scores.append((article_vector.get("_id"), similarity_score))
                     #print("ft Cosine Similarity Score - " + str(article_vector.get("baslik")) + ": ", similarity_score)
-                    st = article_vector.get("scibert", [])
                     similarity_score2 = cosine_similarity(st, user_vector1)
                     similarity_scores2.append((article_vector.get("_id"), similarity_score2))
                     #print("sb Cosine Similarity Score - " + str(article_vector.get("baslik")) + ": ", similarity_score2)   
@@ -132,7 +132,7 @@ def dashboard(request):
                     oneri = f"{oneri},"
                     performans=calculate_precision(ialanlari,oneri)
                     performans1.append(performans)
-                    print("Oneri performansi1:",performans)                                 #!!!bu kisim arayuzde gosterilmeli her makale icin
+                    #print("Oneri performansi1:",performans)                                 #!!!bu kisim arayuzde gosterilmeli her makale icin
 
                 for fso in top_5_scores_with_ids2:
                     oneri=[]
@@ -142,7 +142,7 @@ def dashboard(request):
                     oneri = f"{oneri},"
                     performans=calculate_precision(ialanlari,oneri)
                     performans1.append(performans)
-                    print("Oneri performansi2:",performans)                                 #!!!bu kisim arayuzde gosterilmeli her makale icin
+                    #print("Oneri performansi2:",performans)                                 #!!!bu kisim arayuzde gosterilmeli her makale icin
                 #print(similarity_id)
                 # Kullanici verilerini guncelle
                 users_collection.update_one({'_id': user_data['_id']}, {'$set': {'f_advantages': similarity_id}})
@@ -150,25 +150,43 @@ def dashboard(request):
                 users_collection.update_one({'_id': user_data['_id']}, {'$set': {'precision': performans1}})
                 
                 # Kullanicinin ilgili makalelerini al
+                f_advantages=[]
+                s_advantages=[]
                 f_advantages = user_data.get('f_advantages', [])
                 s_advantages = user_data.get('s_advantages', [])
 
                 # ilgili makaleleri cek
                 f_advantages_articles = makale_collection.find({'_id': {'$in': f_advantages}})
                 s_advantages_articles = makale_collection.find({'_id': {'$in': s_advantages}})
-
                 f_advantages_data = []
+                s_advantages_data = []
+
+                ind1=0
                 for article in f_advantages_articles:
                     article_data = {
                         "kimlik": str(article["_id"]),
                         "baslik": article["baslik"],
                         "icerik": article["icerik"],
-                        "key_icerik": article["key_icerik"]
+                        "key_icerik": article["key_icerik"],
+                        "performans":performans1[ind1],
                     }
-                    # Makale verisini listeye ekle
+                    ind1+=1
                     f_advantages_data.append(article_data)
 
-            return render(request, 'dashboard.html', {'user_data': user_data, "f_advantages_data":f_advantages_data, "s_advantages_articles":s_advantages_articles})   
+
+                ind2=5
+                for article in s_advantages_articles:
+                    article_data1 = {
+                        "kimlik": str(article["_id"]),
+                        "baslik": article["baslik"],
+                        "icerik": article["icerik"],
+                        "key_icerik": article["key_icerik"],
+                        "performans":performans1[ind2],
+                    }
+                    ind2+=1
+                    s_advantages_data.append(article_data1)
+
+            return render(request, 'dashboard.html', {'user_data': user_data, "f_advantages_data":f_advantages_data, "s_advantages_articles":s_advantages_data})   
     
         else:
             # Kullanici verileri bulunamadiysa hata mesaji goster
@@ -216,9 +234,10 @@ def get_top_5_similarity_scores_with_ids(similarity_scores):
     # En yuksek 5 benzerlik skorunu sec
     top_5_similarity_scores = sorted_similarity_scores[:5]
 
-    # Her bir benzerlik skorunun kar��s�na makale ID'sini ekleyerek iki boyutlu bir dizi olu�tur
+    # Her bir benzerlik skorunun karsisina makale ID'sini ekleyerek iki boyutlu bir dizi olustur
+    #print("top5-->",top_5_similarity_scores)
     top_5_similarity_scores_with_ids = [(article_id, similarity_score) for article_id, similarity_score in top_5_similarity_scores]
-
+    #print("top_5-->",top_5_similarity_scores_with_ids)
     return top_5_similarity_scores_with_ids
 
 def articleDetail(request, id):
@@ -233,7 +252,7 @@ def articleDetail(request, id):
 
 def userLogin(request):
     form = AuthenticationForm()
-    #makaleKayit()    #bu kısım tum makaleler yuklenikten sonra yoruma alinmali
+    #makaleKayit()    #bu kisim tum makaleler yuklenikten sonra yoruma alinmali
     if request.method == 'POST':
         email = request.POST.get('email')
         password = request.POST.get('password')
@@ -293,7 +312,7 @@ def updateInterestAreas(request):
             return render(request, 'updateInterestAreas.html', {'user_data': user_data})
         else:
             # Kullanici verileri bulunamadiysa hata mesaji goster
-            messages.error(request, 'Kullanıcı verileri bulunamadı.')
+            messages.error(request, 'Kullanici verileri bulunamadi.')
             return redirect('home')
     else:
         # Kullanici oturumu kapali ise ana sayfaya yonlendir
@@ -311,7 +330,7 @@ def updateInterestAreasForm(request):
                     users_collection.update_one({'email': user_email}, {'$set': {'ilgi_alanlari': ilgi_alanlari}})
                     messages.success(request, 'ilgi alanlari basariyla guncellendi.')
                 else:
-                    messages.error(request, 'Kullanici oturumu bulunamadı.')
+                    messages.error(request, 'Kullanici oturumu bulunamadi.')
             except Exception as e:
                 print("ilgi alanlari guncelleme sonrasinda bir hata olustu:", e)
                 messages.error(request, 'ilgi alanlari guncelleme islemi sonrasinda bir hata olustu.')
@@ -325,6 +344,7 @@ def updateInterestAreasForm(request):
 def preprocess_text(text):
     #print("PREPROCESS İŞLEMİ")
     #text= " ".join(text)  #bu hatali
+    text = f",{text}"
     text = text.replace('\n', ' ').replace('\t', ' ')
     text = text.replace("\\n", ' ').replace("\\t", ' ')
 
@@ -347,6 +367,8 @@ def preprocess_text(text):
 
 def kullaniciVektorF(ilgiAlanlari):#fasttext
     preprocessed_profile = " ".join(preprocess_text(ilgiAlanlari))    # Metin on isleme adimlari uygulandi
+    print("deneme",preprocessed_profile)
+
     user_vector = fasttext_model.get_sentence_vector(preprocessed_profile)
     #print("kullanici vektor:", user_vector)
 
@@ -355,7 +377,7 @@ def kullaniciVektorF(ilgiAlanlari):#fasttext
 
 def kullaniciVektorS(ilgiAlanlari):#SCIBERT
     preprocessed_profile = " ".join(preprocess_text(ilgiAlanlari))    # Metin on isleme adimlari uygulandi
-
+    print("deneme",preprocessed_profile)
     scibert_tokens = scibert_tokenizer.encode(preprocessed_profile, return_tensors="pt", padding=True, truncation=True)
     scibert_output = scibert_model(scibert_tokens)
     user_vector = scibert_output.pooler_output.detach().numpy()[0]
@@ -368,12 +390,8 @@ def makaleKayit():#makaleleri vektorleriyle birlikte mognodbye kayit eder
     dosya_yolu="Inspec/docsutf8/"
     dosya_yolu2="Inspec/keys/"
     makale_collection = db['articles']  # Koleksiyon adi
-    index=0
     for dosya_adi in os.listdir(dosya_yolu):
-        index+=1
-        print(dosya_adi)
-        if index==51:
-            break
+        
         if dosya_adi.endswith(".txt"): 
             
             with open(os.path.join(dosya_yolu, dosya_adi), 'r', encoding='utf-8',errors='ignore') as dosya:
@@ -383,7 +401,8 @@ def makaleKayit():#makaleleri vektorleriyle birlikte mognodbye kayit eder
                 ft_makale_vektor=makaleVektorF(makale_icerik).tolist()
                 sb_makale_vektor=makaleVektorS(makale_icerik).tolist()
                 print("b1") 
-
+                baslikAl=f",{makale_icerik}"
+                baslik=baslikAl.split('\n')[0]
             dosya_adi = dosya_adi[:-4]+".key"
 
             with open(os.path.join(dosya_yolu2, dosya_adi), 'r', encoding='utf-8',errors='ignore') as dosya2:
@@ -394,9 +413,8 @@ def makaleKayit():#makaleleri vektorleriyle birlikte mognodbye kayit eder
                             sb_key_vektor=makaleVektorS(key_icerik).tolist()
                         
                             print("key MongoDB'ye basariyla eklendi.")
-
                             makale_verisi = {
-                                "baslik": dosya_adi,
+                                "baslik": baslik,
                                 "icerik": makale_icerik,
                                 "fasttext":ft_makale_vektor,
                                 "scibert":sb_makale_vektor,
@@ -433,6 +451,23 @@ def cosine_similarity(makaleVektoru, kullaniciVektoru):
     norm2 = np.linalg.norm(kullaniciVektoru)
     similarity = dot_product / (norm1 * norm2)
     return similarity
+
+def precision(user_interests, model_predictions):
+    true_positives = 0
+    false_positives = 0
+    
+    for user, interests in user_interests.items():
+        predictions = model_predictions.get(user, [])
+        
+        for pred in predictions:
+            if pred in interests:
+                true_positives += 1
+            else:
+                false_positives += 1
+                
+    precision = true_positives / (true_positives + false_positives) if (true_positives + false_positives) > 0 else 0
+    return precision
+
 
 def calculate_precision(user_interests, tavsiyEdilenkey):
     true_positives = 0
